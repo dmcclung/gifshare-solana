f25dd7b562419a750f94f89974ac7a91371df570
dmcclung <dylanmcclung@gmail.com>
2021-11-13 00:51:51 -0600

    Updated program id

--- programs/gifshare/src/lib.rs
+++ programs/gifshare/src/lib.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("JBg1GKP7ysjTmkHfqbRxj3Hz5LwxxeEHsw6SeVcJmka8");
 
 #[program]
 pub mod gifshare {
